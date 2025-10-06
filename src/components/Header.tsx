aebddfee0c07e6be8feef8b05232f91baef74668
Carlos Orihuela <caorihuela1975@gmail.com>
2025-10-06 21:04:03 +0200

    Instalacion de librerias y fraemwork

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,11 @@
 
 export default function Header() {
+
+
+
   return (
     <>
-      <header>
-        <div>
-          <h1>Aqui ira el Nombre</h1>
-        </div>
-      </header>
+      
     </>
 
   )
