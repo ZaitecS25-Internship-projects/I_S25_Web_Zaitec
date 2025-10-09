3bf3c4758d51d99ebd2323c1eff5215cb3c127d3
Natalia Calabuig <ncm0020-crypto>
2025-10-09 18:52:02 +0200

    Ajustando contenido

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,25 @@
+import { useState, useEffect } from "react"
+import HeaderDesktop from './HeaderDesktop';
+import HeaderMovil from "./HeaderMovil";
+
 
 export default function Header() {
+    const [modeMobile, setModMobile] = useState<boolean>(false)
 
+    useEffect(() => {
+        const handleResize = () => setModMobile(window.innerWidth < 768);
+        handleResize();
 
+        window.addEventListener("resize", handleResize);
 
-  return (
-    <>
-      
-    </>
+        return () => window.removeEventListener("resize", handleResize)
 
-  )
+    }, [])
+    return (
+        <>
+            <header>
+                {!modeMobile ? <HeaderDesktop /> : <HeaderMovil />}
+            </header>
+        </>
+    )
 }
---
+++ src/router.tsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter } from "react-router-dom"
+import Layout from "./layouts/Layout";
+import IndexPage from "./views/IndexPage";
+import Contacto from "./views/Contacto";
+import Desarrollo from "./views/Desarrollo";
+import Formacion from "./views/Formacion";
+
+export const routerApp = createBrowserRouter([
+
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <IndexPage />
+            },
+            {
+                path: 'contacto',
+                element: <Contacto />
+            },
+            {
+                path: 'formacion',
+                element: <Formacion />
+            },
+            {
+                path: 'desarrollo',
+                element: <Desarrollo />
+            }
+        ]
+    }
+])
