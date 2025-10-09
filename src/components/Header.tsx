a69672d6d2df0722544d0e775b7f547797453599
Carlos Orihuela <caorihuela1975@gmail.com>
2025-10-09 23:14:49 +0200

    borrar comentarios

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,24 +4,23 @@ import HeaderMovil from "./HeaderMovil";
 
 
 export default function Header() {
-    //! Estado para manejar la resolución
+    //Estado para manejar la resolución
     const [modeMobile, setModMobile] = useState<boolean>(false)
 
-    //! Actualizamos en el primer render
+    // Actualizamos en el primer render
     useEffect(() => {
-        //! función que maneja el estado
+        // función que maneja el estado
         const handleResize = () => setModMobile(window.innerWidth < 768);
-        //! llamada a la función para su ejecución inmedieta
+        // llamada a la función para su ejecución inmedieta
         handleResize();
-        //! creando el evento y añadimos la función 
+        // creando el evento y añadimos la función 
         window.addEventListener("resize", handleResize);
-        //! borramos el evento para renders innecesarios
+        // borramos el evento para renders innecesarios
         return () => window.removeEventListener("resize", handleResize)
 
     }, [])
     return (
         <>
-        //! Dependiendo la resolución mostramos su header
             <header>
                 {!modeMobile ? <HeaderDesktop /> : <HeaderMovil />}
             </header>
