25737ce864d31d175b15746ba01ef9ed1bfaf4f1
Carlos Orihuela <caorihuela1975@gmail.com>
2025-10-09 16:55:45 +0200

    Mejorar boton menu

--- src/components/HeaderMovil.tsx
+++ src/components/HeaderMovil.tsx
@@ -38,17 +38,15 @@ export default function HeaderMovil() {
             </div>
 
             <div className='absolute top-5 right-5 z-50 flex flex-col items-end gap-1'>
-                <div className='grid grid-cols-2 justify-items-end gap-1'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 text-orange-400">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
-                </svg>
 
                 <button
                     type='button'
-                    className='block text-white pr-2 '
+                    className='flex flex-col items-center text-white pr-2 '
                     onClick={openNav}
-                >Menu</button>
-                </div>
+                ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-7 text-orange-400">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
+                    </svg><span className='text-sm'>Menu</span></button>
+
 
                 {modalNav && (
                     <nav className=' flex flex-col items-end bg-gray-900 opacity-80 rounded-xl'>
