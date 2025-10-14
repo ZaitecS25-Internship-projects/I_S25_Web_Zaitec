839fd32e9997bfcc2156f34d69e11ef869a5c05b
Carlos Orihuela <caorihuela1975@gmail.com>
2025-10-14 19:59:05 +0200

    Crear el formulario

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,13 +1,29 @@
+import { useState } from "react"
 import { Outlet } from "react-router-dom"
 import Header from "../components/Header"
+import type { userDataType } from "../types"
 
+export const initialForm = {
+nombre: '',
+apellido: '',
+segunapellido: '',
+email: '',
+telefono: '',
+fecha: '',
+comentario: '',
+}
 
 export default function Layout() {
+
+  const [formState, setFormState] = useState<userDataType>(initialForm)
+
   return (
     <>
-        <Header />
-          
-        <Outlet /> 
+      <Header />
+
+      <Outlet
+      context={{formState, setFormState}}
+      />
     </>
   )
 }
