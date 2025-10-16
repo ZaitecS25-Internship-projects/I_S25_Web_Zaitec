2cbe02ced27cfedc69bb4ccb77188f3f6b86f523
Enrique Garrido <egg0023@alu.medac.es>
2025-10-16 13:32:32 +0200

    footer

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import { routerApp } from './router'
+import Footer from "./components/Footer"
 
 //! Cargar el routerApp desde el archivo router
 createRoot(document.getElementById('root')!).render(
