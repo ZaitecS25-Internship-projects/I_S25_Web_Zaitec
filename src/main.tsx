013b83161547328afa179754b13fe2ef150aa199
ncm0020-crypto <ncm0020@alu.medac.es>
2025-10-16 19:44:03 +0200

    Actualización

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import { routerApp } from './router'
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/main
 
 //! Cargar el routerApp desde el archivo router
 createRoot(document.getElementById('root')!).render(
