a9460f9dd5c8eb01b525b809974b8f7630c50c85
Enrique Garrido <egg0023@alu.medac.es>
2025-10-17 12:35:37 +0200

    Update Layout.tsx
    layout con footer añadido

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom"
 import Header from "../components/Header"
-
+import Footer from "../components/Footer"
 
 export default function Layout() {
   return (
     <>
-        <Header />
-          
-        <Outlet /> 
+      <Header />
+      <Outlet />
+      <Footer />
     </>
   )
 }
