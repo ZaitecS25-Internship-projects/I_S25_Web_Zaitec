2bc09c4e326885f37cf29df1e9795b36f6572860
Jorgejmp0057 <jmp0057@alu.medac.es>
2025-10-23 18:59:37 +0200

    Corrijo orden en IndexPage

--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -44,75 +44,17 @@ export default function IndexPage() {
       </section>
 
       <section
-      className="w-6/7 mx-auto"
-      style={{backgroundColor:'yellow'}}
-      id="contacto">
+        className="w-6/7 mx-auto"
+        style={{ backgroundColor: 'yellow' }}
+        id="contacto">
         <Contacto />
       </section>
 
-      <section aria-labelledby="soluciones-heading"
-      id="desarrollo-proyectos">
-        <div className="flex flex-col gap-5 lg:w-4/5 mx-auto p-5 md:pt-25 ">
-          <h2 id='soluciones-heading' className="text-2xl font-semibold md:text-4xl md:w-2/3 lg:text-5xl lg:text-neutral-800 lg:w-3/4"><span className="text-orange-600">Proyectos </span>que generan confianza y resultados</h2>
-          <p className="md:w-2/3 lg:text-2xl lg:w-3/4 text-neutral-600">Explora nuestras soluciones clave, donde convergen innovación, experiencia y resultados excepcionales.</p>
-          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10">
-            <motion.article
-              initial={{ opacity: 0 }}            // empieza invisible y un poco abajo
-              whileInView={{ opacity: 1 }}        // aparece y sube a posición
-              transition={{ duration: 0.9, delay: 0.2, ease: 'easeIn' }}
-              viewport={{ once: true, amount: 0.5 }}   // solo se anima la primera vez, al 50% visible
-              className="flex flex-col gap-2 border border-neutral-400 rounded-lg px-4 py-6 shadow-lg shadow-neutral-400 min-h-35"
-            >
-              <div className="w-full aspect-square overflow-hidden rounded-lg">
-                {/* Este div crea un contenedor cuadrado con bordes redondeados para mantener la forma de la imagen */}
-                <img
-                  src="/img/pepe-martinez.png"
-                  alt="Pepe Martínez y Asociados"
-                  className="transition-transform duration-500 hover:scale-110"//zoom al hacer hover
-                />
-              </div>
-                <h3 className="text-xl font-bold">Pepe Martínez y asociados, bufete de abogados</h3>
-              <p className="text-neutral-600">Posicionamiento SEO, formularios para consejo legal y mucho más.</p>
-            </motion.article>
 
-            <motion.article
-              initial={{ opacity: 0 }}            // empieza invisible y un poco abajo
-              whileInView={{ opacity: 1 }}        // aparece y sube a posición
-              transition={{ duration: 0.9, delay: 0.2, ease: 'easeIn' }}
-              viewport={{ once: true, amount: 0.5 }}   // solo se anima la primera vez, al 50% visible
-              className="flex flex-col gap-2 border border-neutral-400 rounded-lg px-4 py-6 shadow-lg shadow-neutral-400 min-h-35"
-            >
-              <div className="w-full aspect-square overflow-hidden rounded-lg">
-                {/* Este div crea un contenedor cuadrado con bordes redondeados para mantener la forma de la imagen */}
-                <img src="/img/cocinas-con-alma.png"
-                  alt="Cocinas con Alma"
-                  className="transition-transform duration-500 hover:scale-110" /> {/*zoom al hacer hover */}
-              </div><h3 className="text-xl font-bold">Cocinas con Alma</h3>
-              <p className="text-neutral-600">Página con diseño impactante y gran detalle en Proyectos de Cocinas realizados. </p>
-            </motion.article>
-
-            <motion.article
-              initial={{ opacity: 0 }}            // empieza invisible y un poco abajo
-              whileInView={{ opacity: 1 }}        // aparece y sube a posición
-              transition={{ duration: 0.9, delay: 0.2, ease: 'easeIn' }}
-              viewport={{ once: true, amount: 0.5 }}   // solo se anima la primera vez, al 50% visible
-              className="flex flex-col gap-2 border border-neutral-400 rounded-lg px-4 py-6 shadow-lg shadow-neutral-400 min-h-35"
-            >
-              <div className="w-full aspect-square overflow-hidden rounded-lg">
-                {/* Este div crea un contenedor cuadrado con bordes redondeados para mantener la forma de la imagen */}
-               <img src="/img/electro-genio.png"
-                  alt="Electro-genio" 
-                  className="transition-transform duration-500 hover:scale-110" /> {/*zoom al hacer hover */}
-              </div><h3 className="text-xl font-bold">Electrodomésticos ElectroGenio</h3>
-              <p className="text-neutral-600">Una web con tienda incorporada con cientos de referencias y pasarelas de pago (tarjetas, bizum, etc).</p>
-            </motion.article>
-          </div>
-        </div>
-      </section>
 
       <section
-      id="formacion"
-      aria-labelledby="capacitacion-tecnologica-heading" className="min-h-screen flex items-center justify-center">
+        id="formacion"
+        aria-labelledby="capacitacion-tecnologica-heading" className="min-h-screen flex items-center justify-center">
         <div className="flex flex-col gap-5 lg:w-4/5 mx-auto mb-5 p-5 pb-25 md:pt-25">
 
           <h2 id="capacitación-tecnologica-heading" className="text-2xl font-semibold md:text-4xl md:w-2/3 lg:text-5xl lg:text-neutral-800 lg:w-3/4">
@@ -125,7 +67,7 @@ export default function IndexPage() {
 
           {/* CONTENEDOR DE TARJETAS */}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 w-11/12 md:w-4/5 mx-auto mt-10"
-          
+
           >
 
             {/* TARJETA 1 - AWS Services */}
@@ -226,6 +168,65 @@ export default function IndexPage() {
                 </motion.div>
               </a>
             </article>
+          </div>
+        </div>
+      </section>
+      <section aria-labelledby="soluciones-heading"
+        id="desarrollo-proyectos">
+        <div className="flex flex-col gap-5 lg:w-4/5 mx-auto p-5 md:pt-25 ">
+          <h2 id='soluciones-heading' className="text-2xl font-semibold md:text-4xl md:w-2/3 lg:text-5xl lg:text-neutral-800 lg:w-3/4"><span className="text-orange-600">Proyectos </span>que generan confianza y resultados</h2>
+          <p className="md:w-2/3 lg:text-2xl lg:w-3/4 text-neutral-600">Explora nuestras soluciones clave, donde convergen innovación, experiencia y resultados excepcionales.</p>
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10">
+            <motion.article
+              initial={{ opacity: 0 }}            // empieza invisible y un poco abajo
+              whileInView={{ opacity: 1 }}        // aparece y sube a posición
+              transition={{ duration: 0.9, delay: 0.2, ease: 'easeIn' }}
+              viewport={{ once: true, amount: 0.5 }}   // solo se anima la primera vez, al 50% visible
+              className="flex flex-col gap-2 border border-neutral-400 rounded-lg px-4 py-6 shadow-lg shadow-neutral-400 min-h-35"
+            >
+              <div className="w-full aspect-square overflow-hidden rounded-lg">
+                {/* Este div crea un contenedor cuadrado con bordes redondeados para mantener la forma de la imagen */}
+                <img
+                  src="/img/pepe-martinez.png"
+                  alt="Pepe Martínez y Asociados"
+                  className="transition-transform duration-500 hover:scale-110"//zoom al hacer hover
+                />
+              </div>
+              <h3 className="text-xl font-bold">Pepe Martínez y asociados, bufete de abogados</h3>
+              <p className="text-neutral-600">Posicionamiento SEO, formularios para consejo legal y mucho más.</p>
+            </motion.article>
+
+            <motion.article
+              initial={{ opacity: 0 }}            // empieza invisible y un poco abajo
+              whileInView={{ opacity: 1 }}        // aparece y sube a posición
+              transition={{ duration: 0.9, delay: 0.2, ease: 'easeIn' }}
+              viewport={{ once: true, amount: 0.5 }}   // solo se anima la primera vez, al 50% visible
+              className="flex flex-col gap-2 border border-neutral-400 rounded-lg px-4 py-6 shadow-lg shadow-neutral-400 min-h-35"
+            >
+              <div className="w-full aspect-square overflow-hidden rounded-lg">
+                {/* Este div crea un contenedor cuadrado con bordes redondeados para mantener la forma de la imagen */}
+                <img src="/img/cocinas-con-alma.png"
+                  alt="Cocinas con Alma"
+                  className="transition-transform duration-500 hover:scale-110" /> {/*zoom al hacer hover */}
+              </div><h3 className="text-xl font-bold">Cocinas con Alma</h3>
+              <p className="text-neutral-600">Página con diseño impactante y gran detalle en Proyectos de Cocinas realizados. </p>
+            </motion.article>
+
+            <motion.article
+              initial={{ opacity: 0 }}            // empieza invisible y un poco abajo
+              whileInView={{ opacity: 1 }}        // aparece y sube a posición
+              transition={{ duration: 0.9, delay: 0.2, ease: 'easeIn' }}
+              viewport={{ once: true, amount: 0.5 }}   // solo se anima la primera vez, al 50% visible
+              className="flex flex-col gap-2 border border-neutral-400 rounded-lg px-4 py-6 shadow-lg shadow-neutral-400 min-h-35"
+            >
+              <div className="w-full aspect-square overflow-hidden rounded-lg">
+                {/* Este div crea un contenedor cuadrado con bordes redondeados para mantener la forma de la imagen */}
+                <img src="/img/electro-genio.png"
+                  alt="Electro-genio"
+                  className="transition-transform duration-500 hover:scale-110" /> {/*zoom al hacer hover */}
+              </div><h3 className="text-xl font-bold">Electrodomésticos ElectroGenio</h3>
+              <p className="text-neutral-600">Una web con tienda incorporada con cientos de referencias y pasarelas de pago (tarjetas, bizum, etc).</p>
+            </motion.article>
           </div>
         </div>
       </section>
