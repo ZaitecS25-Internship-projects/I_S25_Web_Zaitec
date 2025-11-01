bd90f9d6b434ccb29ca44dedcef9a8d417f876f0
Carlos Orihuela <caorihuela1975@gmail.com>
2025-11-02 00:40:08 +0100

    Formulario y LocalStorage

--- src/components/CardInnovacion.tsx
+++ src/components/CardInnovacion.tsx
@@ -15,7 +15,7 @@ export default function CardInnovacion({ cards }: CardPropsType) {
                     whileInView={{ opacity: 1 }}      
                     transition={{ duration: 1, delay: 0.2, ease: 'easeIn' }}
                     viewport={{ once: true, amount: 0.5 }}   // solo se anima la primera vez, al 50% visible
-                    className={`flex flex-col gap-3 px-3 justify-center min-h-45 ${index < 2 ? 'border-r-3 border-neutral-500' : null}`}
+                    className={`flex flex-col gap-3 px-3 justify-center min-h-45 ${index < 2 ? 'border-r-3 border-neutral-400' : null}`}
                 >
                     <h3 className="text-2xl font-bold text-neutral-600">{card.titulo}</h3>
                     <p className="text-neutral-500">{card.descripcion}</p>
